e96190bce8d07e747c29823932c70a69fb70469e
Talbekk <mt.campbell@outlook.com>
2022-03-02 22:06:20 +0000

    adds basic profile

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Container(
-                height: 100.0,
-                width: 100.0,
-                margin: EdgeInsets.only(left: 30.0),
-                padding: EdgeInsets.all(20.0),
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/profile.jpeg'),
+            ),
+            Text(
+              'Michael Campbell',
+              style: TextStyle(
+                fontSize: 20.0,
                 color: Colors.white,
-                child: Text('Hello'))),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
