5ebb01ead06e978fa841c1654035773c5dd9ab57
Talbekk <mt.campbell@outlook.com>
2022-03-03 22:35:24 +0000

    refactors and styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50.0,
@@ -21,69 +23,61 @@ class MyApp extends StatelessWidget {
               'Michael Campbell',
               style: TextStyle(
                 fontFamily: 'Comforter Brush',
-                fontSize: 30.0,
+                fontSize: 40.0,
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Text(
-              'Junior Dev',
+              'Junior Developer',
               style: TextStyle(
                 fontSize: 20.0,
-                color: Colors.teal.shade100,
+                color: Colors.blueGrey.shade100,
                 letterSpacing: 2.5,
               ),
             ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
-              ),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+44 1234567 89',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
+            SizedBox(
+              height: 20.0,
+              width: 200.0,
+              child: Divider(
+                color: Colors.blueGrey.shade100,
               ),
             ),
-            Container(
-              color: Colors.white,
+            Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    '+44 7746796763',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade900,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            Card(
               margin: EdgeInsets.symmetric(
                 vertical: 10.0,
                 horizontal: 25.0,
               ),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.blueGrey,
+                ),
+                title: Text(
+                  'mt.campbell@outlook.com',
+                  style: TextStyle(
+                    color: Colors.blueGrey.shade900,
+                    fontSize: 20.0,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'mt.campbell@outlook.com',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontSize: 20.0,
-                    ),
-                  )
-                ],
+                ),
               ),
             ),
           ],
